8911a31fa2cba9e74a7e64ba284539403fcea1ca
keshavnagar <keshavnagar887@gmail.com>
2025-01-30 16:22:18 +0530

    but jab || ka use karenge to no student found output milega

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -3,7 +3,9 @@ const Practice = () => {
   return (
     <>
       <div>Interview Question : </div>
-      <p>{Students.length && "no student found"}</p>
+      {/* <p>{Students.length && "no student found"}</p> */}
+      <p>{Students.length || "no student found"}</p> 
+      {/* but jab || ka use karenge to no student found output milega  */}
       {/* above line output is 0 , why its 0 , I give reason in source.md file :  */}
       {/* jab and operator lagayenge tab 0 output milega  */}
       <p>student length : {Students.length}</p>
