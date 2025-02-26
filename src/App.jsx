ab11d94152ec94901bd49117deefcdb1e1ab9466
keshavnagar <keshavnagar887@gmail.com>
2025-02-26 20:54:49 +0530

    add css to the container in app.jsx to look more beautiful section container add padding , max width to look good container

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
-import Card  from "./cmoponents/Card";
+import Card from "./cmoponents/Card";
 // import ProfileCard from "./cmoponents/ProfileCard";
 // import Profile  from "./cmoponents/Profile";
 console.log(React.createElement("h1", null, "hello world"));
 const App = () => {
   return (
     <>
-    <Card />
-    {/* <Profile /> */}
-    </> 
+      <section className="container">
+        <Card />
+        {/* <Profile /> */}
+      </section>
+    </>
   );
-}
+};
 export default App;
